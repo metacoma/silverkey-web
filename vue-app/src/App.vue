fb1bb94071b8dd440c2d6b9f44b008ed25793c71
metacoma <m3tac0ma@gmail.com>
2018-05-29 01:42:14 +0400

    head and download page

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,50 +1,11 @@
 <template>
-  <div class="page-container">
-    <md-app md-waterfall  md-mode="fixed"  >
-      <md-app-toolbar class="md-large md-dense md-primary">
-        <div class="md-toolbar-row">
-          <div class="md-toolbar-section-start">
-            <span class="md-title">Silverkey app</span>
-          </div>
-
-          <div class="md-toolbar-section-end">
-            <md-button class="md-icon-button">
-              <md-icon>more_vert</md-icon>
-            </md-button>
-          </div>
-        </div>
-
-        <div class="md-toolbar-row">
-          <md-tabs class="md-primary">
-            <md-tab id="tab-home" md-label="Home"></md-tab>
-            <md-tab id="tab-download" md-label="Downloads"></md-tab>
-          </md-tabs>
-        </div>
-      </md-app-toolbar>
-
-      <md-app-content style="min-height: 1000vh;">
-<!--
-        <youtube video-id="-3JCESdFNyw" :player-height=800></youtube>
--->
-      </md-app-content>
-    </md-app>
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
-<style lang="scss" scoped>
-  .md-app {
-    max-height: 1200px;
-    border: 1px solid rgba(#000, .12);
-  }
-
-   // Demo purposes only
-</style>
-
 <script>
 export default {
-  name: 'App',
-  data: () => ({
-    menuVisible: false
-  })
+  name: 'App'
 }
 </script>
--- vue-app/src/components/Download.vue
+++ vue-app/src/components/Download.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>{{ msg }}</h1>
+    <div class="Download">
+       <md-tabs md-alignment="centered" md-sync-route=True>
+        <md-tab id="tab-home" md-label="Home" to="/">
+          <youtube video-id="-3JCESdFNyw" :player-height=800></youtube>
+        </md-tab>
+        <md-tab id="tab-download" md-label="Download" to="/download"></md-tab>
+        <md-tab id="tab-about" md-label="About" to="/about" md-disabled></md-tab>
+        <md-tab id="tab-contact" md-label="Contact" to="/contact" md-disabled></md-tab>
+        <md-tab md-alignment="right" id="tab-login" md-label="LOGIN" to="/login" md-disabled></md-tab>
+        <md-tab md-alignment="right" id="tab-signup" md-label="SIGNUP" to="/signup" md-disabled></md-tab>
+      </md-tabs>
+
+    </div>
 </template>
 
 <script>
