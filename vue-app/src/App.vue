fd4ebb7676bbde7fb74e833e8e27f84a532dfe7f
metacoma <m3tac0ma@gmail.com>
2018-05-28 23:10:19 +0400

    delete unused stuff

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="page-container">
-    <md-app md-waterfall  md-mode="fixed-last"  >
+    <md-app md-waterfall  md-mode="fixed"  >
       <md-app-toolbar class="md-large md-dense md-primary">
         <div class="md-toolbar-row">
           <div class="md-toolbar-section-start">
-          <!--
-            <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
-              <md-icon>menu</md-icon>
-            </md-button>
-          -->
             <span class="md-title">Silverkey app</span>
           </div>
 
@@ -26,36 +21,9 @@
           </md-tabs>
         </div>
       </md-app-toolbar>
-      <!--
-      <md-app-drawer :md-active.sync="menuVisible">
-        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
 
-        <md-list>
-          <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Inbox</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
-          </md-list-item>
-        </md-list>
-      </md-app-drawer>
-      -->
-      <md-app-content style="min-height: 1200vh;">
-        <br><br><br><br><br><br>
-        <youtube video-id="-3JCESdFNyw" player-width=800 player-height=1200 ></youtube>
+      <md-app-content style="min-height: 100vh;">
+        <iframe width="560" height="600" src="https://www.youtube.com/embed/ca7T0bXptU8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </md-app-content>
     </md-app>
   </div>
