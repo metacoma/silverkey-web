77079999c7f05deaf3ee26be62afc68c98473d05
metacoma <m3tac0ma@gmail.com>
2018-05-30 12:10:04 +0400

    first /api/v1/team implementation

--- backend/lua/team.lua
+++ backend/lua/team.lua
@@ -1,5 +1,24 @@
 local template = require "resty.template"
 local cjson = require "cjson"
 
-ngx.say(template.render("silverkey.yml.tmpl", { version = "Hello, World!" }))
+local manifest_tmpl = template.compile("silverkey.yml.tmpl")
 
+local manifest = manifest_tmpl {
+  etcd = {
+    version = "3.0.17"
+  }
+}
+
+local cmd = { "/usr/local/bin/kubectl", "--kubeconfig", "/tmp/kubeconfig", "-n", "silverkey", "apply", "-f", "-" }
+local prog = require 'resty.exec'.new(os.getenv("SOCKEXEC_SOCKET"))
+
+local res, err = prog( {
+     argv = cmd,
+     stdin = manifest,
+} )
+
+if (err) then
+     ngx.say(err)
+  else
+     ngx.say(res.stdout)
+end
