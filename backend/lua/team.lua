702c867fc28a317b3351c554d668aeaec153b996
metacoma <m3tac0ma@gmail.com>
2018-05-30 12:20:47 +0400

    dynamic namespace for new deployment

--- backend/lua/team.lua
+++ backend/lua/team.lua
@@ -1,15 +1,19 @@
 local template = require "resty.template"
 local cjson = require "cjson"
+local uuid = require 'resty.jit-uuid'
 
 local manifest_tmpl = template.compile("silverkey.yml.tmpl")
 
 local manifest = manifest_tmpl {
   etcd = {
     version = "3.0.17"
+  },
+  team = {
+    uuid = uuid()
   }
 }
 
-local cmd = { "/usr/local/bin/kubectl", "--kubeconfig", "/tmp/kubeconfig", "-n", "silverkey", "apply", "-f", "-" }
+local cmd = { "/usr/local/bin/kubectl", "--kubeconfig", "/tmp/kubeconfig", "create", "-f", "-" }
 local prog = require 'resty.exec'.new(os.getenv("SOCKEXEC_SOCKET"))
 
 local res, err = prog( {
