9b52ea80b4b578e8de9bc6fba7cd44ba83db1976
metacoma <m3tac0ma@gmail.com>
2018-06-02 10:50:57 +0400

    k8s module

--- backend/lua/team.lua
+++ backend/lua/team.lua
@@ -1,28 +1,36 @@
-local template = require "resty.template"
-local cjson = require "cjson"
-local uuid = require 'resty.jit-uuid'
+local k8s = {}
 
-local manifest_tmpl = template.compile("silverkey.yml.tmpl")
+function k8s.new_backend()
+  local template = require "resty.template"
+  local cjson = require "cjson"
+  local uuid = require 'resty.jit-uuid'
 
-local manifest = manifest_tmpl {
-  etcd = {
-    version = "3.0.17"
-  },
-  team = {
-    uuid = uuid()
+  local manifest_tmpl = template.compile("silverkey.yml.tmpl")
+
+  local manifest = manifest_tmpl {
+    etcd = {
+      version = "3.0.17"
+    },
+    team = {
+      uuid = uuid()
+    }
   }
-}
 
-local cmd = { "/usr/local/bin/kubectl", "--kubeconfig", "/tmp/kubeconfig", "create", "-f", "-" }
-local prog = require 'resty.exec'.new(os.getenv("SOCKEXEC_SOCKET"))
+  local cmd = { "/usr/local/bin/kubectl", "--kubeconfig", "/tmp/kubeconfig", "create", "-f", "-" }
+  local prog = require 'resty.exec'.new(os.getenv("SOCKEXEC_SOCKET"))
 
-local res, err = prog( {
+  local res, err = prog(
+    {
      argv = cmd,
      stdin = manifest,
-} )
+    }
+  )
 
-if (err) then
+  if (err) then
      ngx.say(err)
   else
      ngx.say(res.stdout)
+  end
 end
+
+return k8s
