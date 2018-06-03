c4577388f7ac5ec4a9a30c71513cad635f5832db
metacoma <m3tac0ma@gmail.com>
2018-06-03 16:15:18 +0400

    add basic k8s module

--- backend/lua/etcdsk.lua
+++ backend/lua/etcdsk.lua
@@ -5,18 +5,45 @@ local decodeJSON = require('cjson.safe').decode
 
 local EtcdSk = {}
 
-function EtcdSk:get(key)
+function EtcdSk:getApiCall(namespace)
     local httpc = http.new()
 
     local res, err = httpc:request_uri(
-        self.url .. "/v2/keys/" .. key,
+        self.url .. "/v2/keys/" .. namespace,
         {}
     )
+
+    if not res then
+      return nil, err
+    end
+    return res.body
+end
+
+function EtcdSk:get(key)
+    local res, err = EtcdSk:getApiCall(key)
+
     if not res then
       return nil, err
     end
 
-    return decodeJSON(res.body), nil
+    return decodeJSON(res)["node"]["value"], nil
+end
+
+function EtcdSk:ns2table(namespace)
+  local t = {}
+
+  local res, err = EtcdSk:getApiCall(namespace)
+
+  if not res then
+    return nil, err
+  end
+
+
+  for node in decodeJSON(res)["node"]["nodes"] do
+    t[node["key"]] = node["value"]
+  end
+
+  return t
 end
 
 function EtcdSk:new(url)
